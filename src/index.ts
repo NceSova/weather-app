90e2dff158bfc0dc5d2eb651dfbfded9f2bc1c12
Sova <108209320+NceSova@users.noreply.github.com>
2024-07-01 21:26:04 +0300

    add ui for air condition

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,26 @@ const location = new GeoLocationProvider();
 const ui = new UIRenderer(document);
 const formatter = new OpenWeatherApiFormatter(ui);
 const grid = document.querySelector(".forecast-grid");
+const city = document.querySelector(".city");
+const curTemp = document.querySelector(".cur-temp");
+const logo = document.querySelector(".weather-logo") as HTMLImageElement;
+
 location.getLocationPromise().then((data) => {
-  weatherApi.getWeatherObject(data).then((weather) => {
+  //displayWeather(data);
+});
+
+function displayWeather(location: string) {
+  weatherApi.getWeatherObject(location).then((weather) => {
+    city.textContent = weather.location.name;
+    curTemp.textContent = `${weather.current.temp_c.toFixed()}°`;
+    logo.src = `http:${weather.current.condition.icon.replace(
+      "64x64",
+      "128x128"
+    )}`;
     const hourObjects = weather.forecast.forecastday[0];
     const newGrid = ui.renderHourGrid(
       formatter.getForecastHourObjectArray(hourObjects)
     );
     grid.innerHTML = newGrid.innerHTML;
   });
-});
+}
